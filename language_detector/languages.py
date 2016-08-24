baddb255198c217c5de0607ea13a04365a7d53df
Isaac <isaac1@mail.usf.edu>
2016-08-24 00:34:11 +0000

    finish project 1

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -8,4 +8,30 @@ LANGUAGES = [
         'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
     },
     #  include your extra languages here
+    {
+        'name': 'Portuguese',
+        'common_words': ['os', 'as', 'nós', 'você', 'falar', 'comer', 'beber', 
+        'ser', 'dizer', 'ir', 'estar', 'fazer', 'poder', 'ver', 'querer', 'sair',
+        'deixar', 'passar', 'pedir', 'ler', 'acabar', 'pai', 'mãe', 'mulher', 
+        'irmão', 'ter', 'bom', 'grande', 'melhor', 'próprio', 'velho', 'certo',
+        'último', 'longo', 'novo', 'único', 'antigo', 'belo', 'seguinte', 
+        'preciso', 'meio', 'natural', 'maior', 'triste', 'bonito', 'só']
+    },
+    {
+        'name': 'English',
+        'common_words': ['the', 'we', 'you', 'a', 'an', 'be', 'to', 'of', 'and',
+        'in', 'that', 'have', 'I', 'it', 'for' ] 
+    },
+    {
+        'name': 'German',
+        'common_words': [
+        'das', 'ist', 'du', 'ich', 'nicht', 'die', 'und',
+        'der', 'was', 'wir', 'zu', 'ein', 'er', 'sie', 'mir',
+        'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
+        'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
+        'dich', 'haben', 'für', 'habe', 'da', 'nein',
+        'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
+        'einen', 'kann', 'dem'
+        ]
+    }
 ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -8,4 +8,35 @@ from languages import LANGUAGES
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    words = text.split()
+    count_dict ={}
+    
+    def word_count(words,common_words):
+        count = 0
+        for word in words:
+            if word in common_words:
+                count+=1
+        return count
+        
+    for elem in LANGUAGES:
+        lang = elem['name']
+        com_words = elem['common_words']
+        common_count = word_count(words,com_words)
+        count_dict[lang] = common_count
+    
+    def language_detector(count_dict):
+        max_value = max(count_dict.values())
+        for key in count_dict.keys():
+            if max_value == count_dict[key]:
+                return key
+    
+    
+    return language_detector(count_dict)
+    
+    # dictionary = {'Spanish': 0, 'Portuguese': 0}
+    # for key in dictionary:
+    #     dictionary[key] = word_count(words, common_words)
+        
+    
+    
+    
